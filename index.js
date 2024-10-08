55757363f655ae71f97c019c7cb46d7024b792e4
Satyajeet Singh <satyajeet1152000@gmail.com>
2024-10-09 02:17:20 +0530

    even odd and pattern function implementation

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("this is index file.");
+function findEvenNumbers(arr) {
+    return arr.filter((num) => num % 2 === 0);
+}
+
+function findOddNumbers(arr) {
+    return arr.filter((num) => num % 2 !== 0);
+}
+
+function printPattern(n) {
+    for (let i = 1; i <= n; i++) {
+        console.log("*".repeat(i));
+    }
+}
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log("Even Numbers:", findEvenNumbers(numbers));
+console.log("Odd Numbers:", findOddNumbers(numbers));
+console.log("Pattern:");
+printPattern(5);
